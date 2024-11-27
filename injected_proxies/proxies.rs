f6d00c3d90691b7c3352feccfd967d6fd9eec3ae
shohan-pherones <shohan.sub.56@gmail.com>
2024-11-28 03:58:13 +0600

    strucuture changed

--- injected_proxies/proxies.rs
+++
@@ -1,6 +0,0 @@
-pub fn secure_proxy_config() -> String {
-    let domain = std::env::var("DOMAIN").expect("DOMAIN not set");
-    let protocol = std::env::var("PROTOCOL").unwrap_or("http".to_string());
-    let reverse_proxy_port = std::env::var("REVERSE_PROXY_PORT").unwrap_or("80".to_string());
-    format!("{}://{}:{}/proxy", protocol, domain, reverse_proxy_port)
-}
