2178e981a90d1c2d4631b691d93e47d68a826e40
shohan-pherones <shohan.sub.56@gmail.com>
2024-11-28 06:11:44 +0600

    adding cors

--- dist/app.js
+++ dist/app.js
@@ -12,11 +12,17 @@ const morgan_1 = __importDefault(require("morgan"));
 const global_error_1 = __importDefault(require("./app/errors/global.error"));
 const not_found_error_1 = __importDefault(require("./app/errors/not-found.error"));
 const routes_1 = __importDefault(require("./app/routes"));
-const env_1 = __importDefault(require("./app/config/env"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
-app.use((0, cors_1.default)({ origin: env_1.default.frontend_base_url, credentials: true }));
+app.use((0, cors_1.default)({
+    origin: [
+        "http://localhost:3000",
+        "https://raviosa-frontend.vercel.app",
+        "https://raviosa-frontend.pages.dev",
+    ],
+    credentials: true,
+}));
 app.use((0, helmet_1.default)());
 app.use((0, morgan_1.default)("short"));
 app.use("/api/v1", routes_1.default);
